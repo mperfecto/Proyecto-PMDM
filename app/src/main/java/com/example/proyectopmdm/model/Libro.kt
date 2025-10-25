b8f784f166d52edab54c315d636e699beb7200c2
mperfecto_ <mperfectoarjona@gmail.com>
2025-10-25 19:23:16 +0200

    data class libro

---
+++ app/src/main/java/com/example/proyectopmdm/model/Libro.kt
@@ -0,0 +1,15 @@
+package com.example.spotifyhome.model
+
+import com.example.proyectopmdm.R
+import java.time.LocalDate
+
+data class Libro(
+    val id: Int,
+    val titulo: String,
+    val autor: String,
+    val portada: Int = R.drawable.book,
+    val sinopsis: String,
+    val paginasTotales: Int? = null,
+    val paginasLeidas: Int? = null,
+    val ultimaLectura: LocalDate? = null
+)
--- app/src/main/java/com/example/proyectopmdm/myComponents/TitleHeader.kt
+++ app/src/main/java/com/example/proyectopmdm/myComponents/TitleHeader.kt
@@ -3,12 +3,10 @@ package com.example.proyectopmdm.myComponents
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextOverflow
